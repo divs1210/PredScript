eaf653ff3de4a3c673997f0d45ab190f26cb1502
Divyansh Prakash <divyansh.prakash@yahoo.com>
2023-09-21 13:25:47 +0530

    WIP: custom parser

---
+++ src/custom-parser.js
@@ -0,0 +1,144 @@
+const { Set } = require("immutable");
+
+const TokenType = Set([
+    // Single-character tokens.
+    "LEFT_PAREN", "RIGHT_PAREN", 
+    "LEFT_BRACE", "RIGHT_BRACE",
+    "COMMA", "DOT", "COLON", "SEMICOLON",
+    "MINUS", "PLUS", "SLASH", "STAR",
+    
+    // One or two character tokens.
+    "BANG", "BANG_EQUAL",
+    "EQUAL", "EQUAL_EQUAL",
+    "GREATER", "GREATER_EQUAL",
+    "LESS", "LESS_EQUAL",
+    
+    // Literals.
+    "SYMBOL", "STRING", "NUMBER",
+    
+    // Keywords.
+    "AND", "OR",
+    "IF", "ELSE", 
+    "TRUE", "FALSE",
+    "MULTIFUN", "FN", 
+    "NULL",
+    "LET", "LOOP", "RECUR",
+    "EOF"
+]);
+
+class Token {
+    constructor(type, lexeme, literal, line) {
+        this.type    = type;
+        this.lexeme  = lexeme;
+        this.literal = literal;
+        this.line    = line;
+    }
+    
+    toString() {
+        return type + " " + lexeme + " " + literal;
+    }
+}
+
+// STATIC
+// ==================================
+let hadError = false;
+
+function parseError(line, msg) {
+    report(line, "", msg)
+}
+
+function report(line, where, msg) {
+    console.error(`[line ${line}] Error${where}: ${msg})`);
+    hadError = true;
+}
+// ==================================
+
+class Scanner {
+    constructor(source) {
+        this.source  = source;
+        this.tokens  = [];
+        this.start   = 0;
+        this.current = 0;
+        this.line    = 1;
+    }
+    
+    scanTokens() {
+        while(!this.isAtEnd()) {
+            // We are at the beginning of the next lexeme.
+            this.start = this.current;
+            this.scanToken();
+        }
+        
+        this.tokens.add(new Token("EOF", "", null, line));
+        
+        return this.tokens;
+    }
+    
+    scanToken() {
+        let ch = this.advance();
+        switch (ch) {
+            case '(': this.addToken('LEFT_PAREN');  break;
+            case ')': this.addToken('RIGHT_PAREN'); break;
+            case '{': this.addToken('LEFT_BRACE');  break;
+            case '}': this.addToken('RIGHT_BRACE'); break;
+            case ',': this.addToken('COMMA');       break;
+            case '.': this.addToken('DOT');         break;
+            case ':': this.addToken('COLON');       break;
+            case ';': this.addToken('SEMICOLON');   break;
+            case '-': this.addToken('MINUS');       break;
+            case '+': this.addToken('PLUS');        break;
+            case '*': this.addToken('STAR');        break;
+            case '!': this.addToken(match('=') ? 'BANG_EQUAL'  : 'BANG');      break;
+            case '=': this.addToken(match('=') ? 'EQUAL_EQUAL' : 'EQUAL');     break;
+            case '<': this.addToken(match('=') ? 'LESS_EQUAL'  : 'LESS');      break;
+            case '>': this.addToken(match('=') ? 'GREATER_EQUAL' : 'GREATER'); break;
+            case '/':
+                if (this.match('/')) {
+                    // comment
+                    while (this.peek() != '\n' && !this.isAtEnd())
+                    this.advance();
+                } else {
+                    // slash
+                    this.addToken('SLASH');
+                } break;
+            // whitespace
+            case ' ' :
+            case '\r':
+            case '\t': break;
+            case '\n': line++; break;
+            // unknown
+            default: parseError(line, `Unexpected character: ${ch}`);
+        }
+    }
+    
+    peek() {
+        if (this.isAtEnd()) 
+        return '\0';
+        
+        return this.source[current];
+    }
+    
+    advance() {
+        return this.source[this.current++];
+    }
+    
+    match(expectedChar) {
+        if (this.isAtEnd())
+        return false;
+        if (this.source[current] != expectedChar) 
+        return false;
+        
+        current++;
+        return true;
+    }
+    
+    addToken(type, literal) {
+        literal = literal || null;
+        text = source.substring(start, current);
+        tokens.add(new Token(type, text, literal, line));
+    }
+    
+    isAtEnd() {
+        return this.current >= this.source.length;
+    }
+}
