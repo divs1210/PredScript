28f7cbdf06c58b3189104a3b949db67f53073047
Divyansh Prakash <divyansh.prakash@yahoo.com>
2023-09-22 03:41:15 +0530

    update custom-parser.js

--- src/custom-parser.js
+++ src/custom-parser.js
@@ -1,5 +1,6 @@
 const combinators = require("parjs/combinators");
-const { floatParser, symbolParser, booleanParser, stringParser, blockExprParser } = require("./parsers");
+const { floatParser, symbolParser, booleanParser, stringParser, blockExprParser, binaryExprParser } = require("./parsers");
+const { whitespace } = require('parjs');
 
 const exprParser =
     // numbers
@@ -10,8 +11,12 @@ const exprParser =
     .pipe(combinators.or(symbolParser))
     // strings
     .pipe(combinators.or(stringParser))
+    // binary expressions
+    .pipe(combinators.or(binaryExprParser))
     // block expressions
-    .pipe(combinators.or(blockExprParser));
+    .pipe(combinators.or(blockExprParser))
+    // allow extra spaces
+    .pipe(combinators.between(whitespace()));
 
 module.exports = {
     parseExpr: (code) => exprParser.parse(code)
