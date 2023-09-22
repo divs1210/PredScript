0cd1ea68167721dc4357e38fc734185fb12b69c8
Divyansh Prakash <divyansh.prakash@yahoo.com>
2023-09-22 23:14:37 +0530

    parseExpr

--- src/peg-parser.js
+++ src/peg-parser.js
@@ -1,11 +1,9 @@
 const fs = require('fs');
 const peg = require("pegjs");
-const { pprint } = require('./util');
 
 const grammar = fs.readFileSync('src/grammar.pegjs', 'utf8');
 const parse = peg.generate(grammar).parse;
-
-let parseExpr = null;
+let parseExpr = (code) => parse(code).value[0];
 
 module.exports = {
     parse,
