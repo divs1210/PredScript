7bc2cb4fd841bf4ebbbdbbca585a0457cc83d418
Divyansh Prakash <divyansh.prakash@yahoo.com>
2023-09-23 05:48:33 +0530

    WIP: new multi

--- src/new-multi.js
+++ src/new-multi.js
@@ -51,60 +51,39 @@ function derive(parent, child) {
     }
 }
 
-
-// multimethods
+// MultiMethods
 // ============
-class MultiMethod extends Function {
-    constructor(name, dispatchFn) {
+class MultiMethod {
+    constructor(name) {
         // hack to make objects callable:
         // https://stackoverflow.com/a/40878674/1163490
         super('...args', 'return this.__self__.__call__(...args)');
         var self = this.bind(this);
         this.__self__ = self;
 
-        self.mName = name;
-        self.vTable = Map();
-        self.dispatchFn = dispatchFn;
-        self.defaultImplementation = null;
-
+        self.name = name;
+        self.impls = Map();
+        
         return self;
     }
 
-    implement(dispatchVal, f) {
-        this.vTable = this.vTable.set(dispatchVal, f);
+    implement(argTypes, impl) {
     }
 
-    setDefault(implementation) {
-        this.defaultImplementation = implementation;
+    setDefault(impl) {
+        this.defaultImplementation = impl;
     }
 
-    implementation(dispatchVal) {
-        // TODO: work with hierarchies better
-        // find the implementation for
-        // lowest common ancestors
-        let dispatchOn = dispatchVal;
-        let impl = this.vTable.get(dispatchOn);
-
-        while(isNull(impl) && !is(NotFound, dispatchOn)) {
-            dispatchOn = parentOf(dispatchVal);
-            impl = this.vTable.get(dispatchOn);
-        }
-
+    implementation(argTypes) {
+        let impl = null;
         return !isNull(impl)?
             impl :
             this.defaultImplementation;
     }
-
     __call__(...args) {
-        let dispatchVal = this.dispatchFn(...args);
-        let impl = this.implementation(dispatchVal);
-
-        if(!isNull(impl))
-            return impl(...args);
-
-        throw new Error(`No implementation of ${this.mName} found for dispatch: ${dispatchVal}!`);
     }
 }
+
 
 module.exports = {
     parentOf,
