8c0e5fa48437df9c8f25777b524d9f47f61f10a2
Divyansh Prakash <divyansh.prakash@yahoo.com>
2023-09-25 01:10:46 +0530

    Use ncc to compile to a single file

--- compile.js
+++ compile.js
@@ -1,15 +1,28 @@
 const path = require('node:path'); 
 const fs = require('fs');
 const { compile } = require('./src/compiler');
+const { exec } = require("child_process");
 
 let [_, __, fileName] = process.argv;
 let filePath = path.join(__dirname, fileName);
 let code = fs.readFileSync(filePath, 'utf8');
 let compiledCode = compile(code);
 
-fs.writeFile('out.js', compiledCode, (err) => {
-    if(err)
-        console.error(err);
-    else
-        console.log("Compiled code in: ./out.js");
+exec('mkdir -p dist', () => {
+    fs.writeFile('./dist/index.temp.js', compiledCode, (err) => {
+        if(err)
+            console.error(err);
+        else {
+            console.log("Intermediate output in: ./dist/index.temp.js");
+    
+            exec("ncc build ./dist/index.temp.js -o dist", (error, stdout, stderr) => {
+                if (error)
+                    console.log(`error: ${error.message}`);
+                else if (stderr)
+                    console.log(`stderr: ${stderr}`);
+                else
+                    console.log(`stdout: ${stdout}`);
+            });
+        }
+    });
 });
