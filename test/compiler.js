f7a80b2bd3f181f5483253d09ee6a332fde3fe0f
Manisha Pillai <pillaimanisha77@gmail.com>
2023-09-25 00:29:22 +0530

    let with typecheck
    * let with typecheck
    
    * add example

--- test/compiler.js
+++ test/compiler.js
@@ -118,6 +118,6 @@ function inc(x: isInt): isInt {
 // Let stmt
 // ========
 assert(is(
-    'let a = Real(1.2);',
+    `let a = _apply(as, List([isReal, Real(1.2)]));`,
     compileExpr('let a: isReal = 1.2;')
 ));
