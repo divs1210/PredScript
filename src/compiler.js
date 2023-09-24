4e25c2534ea1600b29459e7999c6cd1afaa10fbd
Divyansh Prakash <divyansh.prakash@yahoo.com>
2023-09-24 05:57:20 +0530

    better compiler errors

--- src/compiler.js
+++ src/compiler.js
@@ -13,7 +13,7 @@ function compileLiteral(node) {
         case 'null':
             return 'null';
         default: {
-            console.error(`Unhandled literal: ${prettify(val)}`);
+            console.error(`Unhandled literal: ${prettify(val)} at ${prettify(node.loc)}`);
             return '????';
         }
     }
@@ -40,7 +40,7 @@ function compileBinaryExpression(node) {
         let compiledRight = compileAST(right);    
         return `_apply(${fn}, List([${compiledLeft}, ${compiledRight}]))`;   
     } else {
-        console.error(`Unhandled binary expression: ${prettify(node)}`);
+        console.error(`Unhandled binary expression: ${prettify(node)} at ${prettify(node.loc)}`);
         return '????';
     }
 }
