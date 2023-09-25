0c73b7913cd61bbf7ea9502bf1924dcc1d5f545f
Divyansh Prakash <divyansh.prakash@yahoo.com>
2023-09-25 19:24:46 +0530

    Allow Comments
    Strip comments before parsing
    
    ---------
    
    Co-authored-by: Manisha Pillai <pillaimanisha77@gmail.com>

--- compile.js
+++ compile.js
@@ -8,21 +8,33 @@ let filePath = path.join(__dirname, fileName);
 let code = fs.readFileSync(filePath, 'utf8');
 let compiledCode = compile(code);
 
-exec('mkdir -p dist', () => {
-    fs.writeFile('./dist/index.temp.js', compiledCode, (err) => {
-        if(err)
-            console.error(err);
-        else {
-            console.log("Intermediate output in: ./dist/index.temp.js");
-    
-            exec("ncc build ./dist/index.temp.js -o dist", (error, stdout, stderr) => {
-                if (error)
-                    console.log(`error: ${error.message}`);
-                else if (stderr)
-                    console.log(`stderr: ${stderr}`);
-                else
-                    console.log(`stdout: ${stdout}`);
-            });
-        }
-    });
+exec('npm i @vercel/ncc -g', (nccErr, nccStdout, nccStderr) => {
+    if (nccErr)
+        console.error(nccErr.message);
+    else if (nccStderr)
+        console.error(nccStderr);
+    else {
+        exec('mkdir -p dist', (dirError, dirStdout, dirStderr) => {
+            if (dirError || dirStderr)
+                console.error('Could not create ./dist directory!');
+            else {
+                fs.writeFile('./dist/index.temp.js', compiledCode, (err) => {
+                    if(err)
+                        console.error(err);
+                    else {
+                        console.log("Intermediate output in: ./dist/index.temp.js");
+                
+                        exec("ncc build ./dist/index.temp.js -o dist", (error, stdout, stderr) => {
+                            if (error)
+                                console.error(error.message);
+                            else if (stderr)
+                                console.error(stderr);
+                            else
+                                console.log(stdout);
+                        });
+                    }
+                });
+            }
+        });
+    }
 });
