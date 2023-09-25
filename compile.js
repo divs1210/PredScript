d6b0d89df6ff4ae850085f0fcf060f2b636804c4
Manisha Pillai <pillaimanisha77@gmail.com>
2023-09-25 19:02:15 +0530

    Fix command line compiler to install ncc

--- compile.js
+++ compile.js
@@ -8,21 +8,33 @@ let filePath = path.join(__dirname, fileName);
 let code = fs.readFileSync(filePath, 'utf8');
 let compiledCode = compile(code);
 
-exec('mkdir -p dist', () => {
-    fs.writeFile('./dist/index.temp.js', compiledCode, (err) => {
-        if(err)
-            console.error(err);
-        else {
-            console.log("Intermediate output in: ./dist/index.temp.js");
-    
-            exec("ncc build ./dist/index.temp.js -o dist", (error, stdout, stderr) => {
-                if (error)
-                    console.log(`error: ${error.message}`);
-                else if (stderr)
-                    console.log(`stderr: ${stderr}`);
-                else
-                    console.log(`stdout: ${stdout}`);
-            });
-        }
-    });
+exec('npm i @vercel/ncc -g', (nccErr, nccStdout, nccStderr) => {
+    if (nccErr)
+        console.log(`error: ${error.message}`);
+    else if (nccStderr)
+        console.log(`stderr: ${stderr}`);
+    else {
+        exec('mkdir -p dist', (dirError, dirStdout, dirStderr) => {
+            if (dirError)
+                console.error('Could not creats ./dist directory!');
+            else {
+                fs.writeFile('./dist/index.temp.js', compiledCode, (err) => {
+                    if(err)
+                        console.error(err);
+                    else {
+                        console.log("Intermediate output in: ./dist/index.temp.js");
+                
+                        exec("ncc build ./dist/index.temp.js -o dist", (error, stdout, stderr) => {
+                            if (error)
+                                console.error(`error: ${error.message}`);
+                            else if (stderr)
+                                console.error(`stderr: ${stderr}`);
+                            else
+                                console.log(`stdout: ${stdout}`);
+                        });
+                    }
+                });
+            }
+        });
+    }
 });
