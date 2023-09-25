267275a29b3e5e2a84e7cc2d7e7a365d9fbffd97
Manisha Pillai <pillaimanisha77@gmail.com>
2023-09-25 18:54:02 +0530

    Strip comments before parsing

--- src/parser.js
+++ src/parser.js
@@ -2,8 +2,16 @@ const fs = require('fs');
 const peg = require("pegjs");
 
 const grammar = fs.readFileSync('src/grammar.pegjs', 'utf8');
-const parse = peg.generate(grammar).parse;
-let parseExpr = (code) => parse(code).value[0];
+const _parse = peg.generate(grammar).parse;
+
+const stripComments = (code) =>
+    code
+    .split(/[\n\r]/)
+    .map(line => line.replace(/\/\/.*/, ''))
+    .join('\n');
+
+const parse = (code) => _parse(stripComments(code));
+const parseExpr = (code) => parse(code).value[0];
 
 module.exports = {
     parse,
