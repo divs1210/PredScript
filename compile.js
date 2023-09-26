b7ad3aca79db6860248ee335e4d91fdf45149904
Manisha Pillai <pillaimanisha77@gmail.com>
2023-09-26 22:06:39 +0530

    Prepare for publishing on npm
    * make psc command
    
    * export builtins
    
    * added keywords
    
    * update name in package.json

--- compile.js
+++ compile.js
@@ -1,11 +1,13 @@
+#!/usr/bin/env node
+
 const path = require('node:path'); 
 const fs = require('fs');
 const { compile } = require('./src/compiler');
 const { exec } = require("child_process");
 
 let [_, __, fileName] = process.argv;
-let filePath = path.join(__dirname, fileName);
-let code = fs.readFileSync(filePath, 'utf8');
+
+let code = fs.readFileSync(fileName, 'utf8');
 let compiledCode = compile(code);
 
 exec('npm i @vercel/ncc -g', (nccErr, nccStdout, nccStderr) => {
@@ -18,13 +20,13 @@ exec('npm i @vercel/ncc -g', (nccErr, nccStdout, nccStderr) => {
             if (dirError || dirStderr)
                 console.error('Could not create ./dist directory!');
             else {
-                fs.writeFile('./dist/index.temp.js', compiledCode, (err) => {
+                fs.writeFile('dist/index.temp.js', compiledCode, (err) => {
                     if(err)
                         console.error(err);
                     else {
-                        console.log("Intermediate output in: ./dist/index.temp.js");
+                        console.log('Intermediate output in: dist/index.temp.js');
                 
-                        exec("ncc build ./dist/index.temp.js -o dist", (error, stdout, stderr) => {
+                        exec('ncc build dist/index.temp.js -o dist', (error, stdout, stderr) => {
                             if (error)
                                 console.error(error.message);
                             else if (stderr)
--- index.js
+++
@@ -1,6 +0,0 @@
-const { compile, compileExpr } = require('./src/compiler');
-
-module.exports = {
-    compile,
-    compileExpr
-};
